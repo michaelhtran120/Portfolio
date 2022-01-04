8a5d43ad7d4bb01f5674a1f06accdc2831583f79
Michael Tran <michaelhtran120@gmail.com>
2022-01-04 14:33:17 -0800

    navbar component completed

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Jumbotron } from "react-bootstrap";
+
+const Hero = () => {
+    return <Jumbotron></Jumbotron>;
+};
+
+export default Hero;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarComponent from "../Navbar/NavbarComponent";
+
+const Main = () => {
+    return (
+        <div>
+            <NavbarComponent />
+        </div>
+    );
+};
+
+export default Main;

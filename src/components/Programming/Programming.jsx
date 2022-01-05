7343304a21c0529ffccccef178d8a72b2cc8aaf8
Michael Tran <michaelhtran120@gmail.com>
2022-01-04 16:47:29 -0800

    programming section start

--- src/components/Programming/Programming.jsx
+++ src/components/Programming/Programming.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import styles from "./Programming.module.scss";
+import marcipeSPA from "../../assets/marecipe-spa.png";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 const Programming = () => {
     return (
-        <div>
-            PROGRAMMING
-        </div>
-    )
-}
+        <section>
+            <Container className='p-5 bg-dark text-center' id='work' fluid>
+                <h1 className='text-light'>Programming Experience</h1>
+                <Container className='my-5'>
+                    <Row>
+                        <Col xs={12} sm={8} xl={4}>
+                            <Card className={styles.card}>
+                                <Card.Img variant='top' src={marcipeSPA} />
+                                <Card.Body className={styles.cardBody}>
+                                    <Card.Title className={styles.cardTitle}>MaRecipe SPA</Card.Title>
+                                    <Card.Text>
+                                        Macro Recipe single page application that allows users to granularly control their macro nutrients.
+                                    </Card.Text>
+                                    <Button
+                                        href='https://github.com/michaelhtran120/MaRecipe-SPA'
+                                        target='_blank'
+                                        variant='outline-primary'
+                                        className={styles.btnOutline}
+                                    >
+                                        Github Repository
+                                    </Button>
+                                </Card.Body>
+                                <Card.Footer>Toolkit: React | TypeScript | Bootstrap | Sass | Jest</Card.Footer>
+                            </Card>
+                        </Col>
+                        <Col xs={12} sm={8} xl={4}>
+                            <Card>
+                                <Card.Img variant='top' src='' />
+                                <Card.Body>
+                                    <Card.Title>Weather Application</Card.Title>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant='primary'>Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </Container>
+        </section>
+    );
+};
 
-export default Programming
+export default Programming;

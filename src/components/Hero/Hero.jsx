b6bd4f98343ae71fd2527faa84ad5ab367447475
Michael Tran <michaelhtran120@gmail.com>
2022-01-13 14:56:16 -0800

    minor ui/css updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import styles from "./Hero.module.scss";
 import { Container } from "react-bootstrap";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import styles from "./Hero.module.scss";
 
 const Hero = () => {
-    return (
-        <Container className={styles.hero} fluid>
-            <h1 className='text-light'>Hello! I'm Michael Tran</h1>
-            <h2 className='text-light'>Front End Web Developer Designer</h2>
-        </Container>
-    );
+  return (
+    <>
+      <Container className={styles.hero} fluid>
+        <h1 className="text-light">Hello! I'm Michael Tran</h1>
+        <h2 className="text-light">Front End Web Developer Designer</h2>
+      </Container>
+    </>
+  );
 };
 
 export default Hero;
--- src/components/Programming/Programming.jsx
+++ src/components/Programming/Programming.jsx
@@ -7,42 +7,40 @@ import styles from "./Programming.module.scss";
 
 const Programming = () => {
   return (
-    <Container className="p-md-5 p-xs-0 pt-5 pb-5 bg-dark" id="work" fluid>
+    <Container className="p-md-5 p-0 pt-5 pb-5 bg-dark" id="work" fluid>
       <h1 className="text-light text-center">Programming Work</h1>
-      <Container fluid className="my-5">
-        <Row className="gx-2 gx-lg-3 gy-lg-4 gy-2 justify-content-center justify-content-lg-start">
-          {pdata.map((project) => {
-            return (
-              <Col key={project.id} xs={10} sm={6} lg={4} xl={3}>
-                <Card className={styles.card}>
-                  <LazyLoadImage className={styles.img} src={project.image} alt={`${project.title} screen shot`} effect="blur" />
-                  <Card.Body className={styles.cardBody}>
-                    <Card.Title className={styles.cardTitle}>{project.title}</Card.Title>
-                    <Card.Text>{project.description}</Card.Text>
-                    <Button href={project.repoUrl} target="_blank" variant="outline-primary" className={styles.btnOutline}>
-                      Github Repo
+      <Row className="gx-2 gx-lg-3 gy-lg-4 gy-2 my-5 mx-0 justify-content-center justify-content-lg-start">
+        {pdata.map((project) => {
+          return (
+            <Col key={project.id} xs={10} sm={6} lg={4} xl={3}>
+              <Card className={styles.card}>
+                <LazyLoadImage className={styles.img} src={project.image} alt={`${project.title} screen shot`} effect="blur" />
+                <Card.Body className={styles.cardBody}>
+                  <Card.Title className={styles.cardTitle}>{project.title}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Button href={project.repoUrl} target="_blank" variant="outline-primary" className={styles.btnOutline}>
+                    Github Repo
+                  </Button>
+                  {project.liveUrl ? (
+                    <Button href={project.liveUrl} target="_blank" variant="outline-primary" className={`${styles.btnOutline} ms-2`}>
+                      Live Demo
                     </Button>
-                    {project.liveUrl ? (
-                      <Button href={project.liveUrl} target="_blank" variant="outline-primary" className={`${styles.btnOutline} ms-2`}>
-                        Live Demo
-                      </Button>
-                    ) : null}
-                  </Card.Body>
-                  <Card.Footer>
-                    {project.toolkit.map((tool, index) => {
-                      if (project.toolkit.lastIndexOf(tool) === project.toolkit.length - 1) {
-                        return <span key={index}> {tool}</span>;
-                      } else {
-                        return <span key={index}> {tool} -</span>;
-                      }
-                    })}
-                  </Card.Footer>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+                  ) : null}
+                </Card.Body>
+                <Card.Footer>
+                  {project.toolkit.map((tool, index) => {
+                    if (project.toolkit.lastIndexOf(tool) === project.toolkit.length - 1) {
+                      return <span key={index}> {tool}</span>;
+                    } else {
+                      return <span key={index}> {tool} -</span>;
+                    }
+                  })}
+                </Card.Footer>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </Container>
   );
 };

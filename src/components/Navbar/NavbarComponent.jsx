ecafbde7aaa6fa838fcec72c85b01c253e700293
Michael Tran <michaelhtran120@gmail.com>
2022-01-17 14:55:33 -0800

    fix navigation bar mobile

--- src/components/Navbar/NavbarComponent.jsx
+++ src/components/Navbar/NavbarComponent.jsx
@@ -2,77 +2,68 @@ import React from "react";
 import logo from "../../assets/Logo.jpg";
 import styles from "./Navbar.module.scss";
 import * as Icon from "react-bootstrap-icons";
-import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const NavbarComponent = () => {
     return (
-        <Navbar sticky='top' bg='light' expand={false}>
+        <Navbar sticky='top' bg='light' expand='md'>
             <Container fluid>
                 <Navbar.Brand href='#'>
                     <img src={logo} className={styles.logo} alt='logo' />
                 </Navbar.Brand>
-                <Nav className='d-none d-md-flex flex-row justify-content-end flex-grow-1 align-items-center'>
-                    <a href='#work' className={styles.navLink}>
-                        Work
-                    </a>
-                    <a href='#about' className={styles.navLink}>
-                        Contact
-                    </a>
-                    <a href='https://github.com/michaelhtran120' target='_blank' rel='noopener noreferrer' className={`${styles.navLinkIcon}`}>
-                        <Icon.Github size={28} />
-                    </a>
-                    <a
-                        href='https://www.linkedin.com/in/michaelhtran120/'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className={`${styles.navLinkIcon}`}
-                    >
-                        <Icon.Linkedin size={28} />
-                    </a>
-                </Nav>
-                {/* Mobile Nav */}
-                <Navbar.Toggle className='d-md-none' aria-controls='offcanvasNavbar' />
-                <Navbar.Offcanvas id='offcanvasNavbar' aria-labelledby='offcanvasNavbarLabel' placement='end'>
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id='offcanvasNavbarLabel'>
-                            <img src={logo} className={styles.logo} alt='logo' />
-                        </Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body className='d-flex flex-column justify-content-between'>
-                        <Nav className='justify-content-end pe-3'>
-                            <a href='#work' className={styles.navLink}>
-                                Work
-                            </a>
-                            <a href='#about' className={styles.navLink}>
-                                About
-                            </a>
-                            <a href='#contact' className={styles.navLink}>
-                                Contact
-                            </a>
-                        </Nav>
-                        <Nav className='flex-row justify-content-around'>
-                            <a
-                                href='https://github.com/michaelhtran120'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                className={`${styles.navLinkIcon}`}
-                            >
-                                <Icon.Github size={28} />
-                            </a>
-                            <a
-                                href='https://www.linkedin.com/in/michaelhtran120/'
-                                target='_blank'
-                                rel='noopener noreferrer'
-                                className={`${styles.navLinkIcon}`}
-                            >
-                                <Icon.Linkedin size={28} />
-                            </a>
-                        </Nav>
-                    </Offcanvas.Body>
-                </Navbar.Offcanvas>
+                <Navbar.Toggle className='d-md-none' aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className="me-auto align-items-center">
+                        <a href='#work' className={styles.navLink}>
+                            Work
+                        </a>
+                        <a href='#about' className={styles.navLink}>
+                            Contact
+                        </a>
+                    </Nav>
+
+                    <Nav className="ms-auto flex-row justify-content-center">
+                        <a
+                            href='https://github.com/michaelhtran120'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className={`${styles.navLinkIcon}`}
+                        >
+                            <Icon.Github size={28} />
+                        </a>
+                        <a
+                            href='https://www.linkedin.com/in/michaelhtran120/'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className={`${styles.navLinkIcon}`}
+                        >
+                            <Icon.Linkedin size={28} />
+                        </a>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
 };
 
 export default NavbarComponent;
+
+// <Nav className='d-none d-md-flex flex-row justify-content-end flex-grow-1 align-items-center'>
+{/* <a href='#work' className={styles.navLink}>
+    Work
+</a>
+<a href='#about' className={styles.navLink}>
+    Contact
+</a> */}
+{/* <a href='https://github.com/michaelhtran120' target='_blank' rel='noopener noreferrer' className={`${styles.navLinkIcon}`}>
+    <Icon.Github size={28} />
+</a> */}
+{/* <a
+    href='https://www.linkedin.com/in/michaelhtran120/'
+    target='_blank'
+    rel='noopener noreferrer'
+    className={`${styles.navLinkIcon}`}
+>
+    <Icon.Linkedin size={28} />
+</a> */}
+// </Nav>

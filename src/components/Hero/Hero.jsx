1894d69629580fcdbc388a61ceafacaa11a7cd2c
Michael Tran <michaelhtran120@gmail.com>
2022-02-04 10:27:34 -0800

    slight button animation and increase dark opacity in hero for ledgibility

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
   return (
     <>
       <Container className={styles.hero} fluid>
+        <div className={styles['overlay']}></div>
         <h1 className="text-light">Hello! I'm Michael Tran</h1>
         <h2 className="text-light">Front End Web Developer Designer</h2>
       </Container>

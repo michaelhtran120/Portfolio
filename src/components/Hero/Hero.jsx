afd65dff2acaf7aa678f2573daebc22026d42661
Michael Tran <michaelhtran120@gmail.com>
2022-02-18 08:42:46 -0800

    hero section animations and scrolldown button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,24 @@
+// React imports & library imports
 import React from "react";
 import { Container } from "react-bootstrap";
-import "react-lazy-load-image-component/src/effects/blur.css";
+import ScrollAnimation from "react-animate-on-scroll";
+
+// CSS import
 import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <>
       <Container className={styles.hero} fluid>
-        <div className={styles["overlay"]}></div>
-        <h1 className="text-light">Hello! I'm Michael Hoan Tran</h1>
-        <h2 className="text-light">Front End Web Developer Designer</h2>
+        <div className={styles["overlay"]} />
+        <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutRight" duration={1.5} delay={500}>
+          <h1 className="text-light">Hello! I'm Michael Hoan Tran</h1>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutLeft" duration={1.5} delay={500} className="text-light">
+          <h2>A Front End Developer</h2>
+          {/* <h2>UX Designer</h2> */}
+        </ScrollAnimation>
+        <a href="#work" className="scrolldown">&#8671;</a>
       </Container>
     </>
   );

84567451d5d53d9f97d2f0fa375a4fac69af34e7
Michael Tran <michaelhtran120@gmail.com>
2022-02-18 09:26:50 -0800

    added animation to programming/design section

--- src/components/Design/Design.jsx
+++ src/components/Design/Design.jsx
@@ -1,9 +1,17 @@
+// React & Library imports
 import React, { useState } from "react";
-import dData from "./data.js";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import ScrollAnimation from "react-animate-on-scroll";
+
+// Style imports
 import styles from "./Design.module.css";
+
+// Data imports
+import dData from "./data.js";
+
+// Component imports
 const AbodeModal = React.lazy(() => import("./AbodeModal/AbodeModal"));
 const PibuModal = React.lazy(() => import("./PibuModal/PibuModal"));
 
@@ -26,22 +34,26 @@ const Design = () => {
   return (
     <section>
       <Container className="p-md-5 p-xs-0 pt-5 pb-5 bg-light" id="work" fluid>
-        <h1 className={styles.designTitle}>Design Work</h1>
+        <ScrollAnimation animateIn="fadeInDown" animateOut="fadeOutDown" duration={1.5}>
+          <h1 className={styles.designTitle}>Design Work</h1>
+        </ScrollAnimation>
         <Container className="my-5">
           <Row className="justify-content-center gy-2">
             {dData.map((project, index) => {
               return (
                 <Col key={index} xs={10} md={6} lg={4}>
-                  <Card>
-                    <LazyLoadImage src={project.image} alt={`${project.title} cover`} effect="blur" width="100%" />
-                    <Card.Body>
-                      <Card.Title>{project.title}</Card.Title>
-                      <Card.Text>{project.description}</Card.Text>
-                      <Button variant="outline-primary" onClick={btnHandler(project)} className={styles.outlineBtn}>
-                        Project Overview
-                      </Button>
-                    </Card.Body>
-                  </Card>
+                  <ScrollAnimation animateIn={project.animateIn} duration={1} delay={1000}>
+                    <Card>
+                      <LazyLoadImage src={project.image} alt={`${project.title} cover`} effect="blur" width="100%" />
+                      <Card.Body>
+                        <Card.Title>{project.title}</Card.Title>
+                        <Card.Text>{project.description}</Card.Text>
+                        <Button variant="outline-primary" onClick={btnHandler(project)} className={styles.outlineBtn}>
+                          Project Overview
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </ScrollAnimation>
                 </Col>
               );
             })}
--- src/components/Design/data.js
+++ src/components/Design/data.js
@@ -5,12 +5,14 @@ const dData = [
   {
     title: "Pibu",
     image: pibu,
-    description: "Design website and branding for client skin care product"
+    description: "Design website and branding for client skin care product",
+    animateIn: "rotateInUpLeft"
   },
   {
     title: "Abode",
     image: abode,
-    description: "Design a responsive application for users to sell/rent homes."
+    description: "Design a responsive application for users to sell/rent homes.",
+    animateIn: "rotateInUpRight"
   }
 ];
 
